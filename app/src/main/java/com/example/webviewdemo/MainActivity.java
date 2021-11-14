ef7311d157067d71d371b3dbc675b08379dd9319
chents <chentaishan@noboauto.com>
2021-11-14 23:09:37 +0800

    init

--- app/src/main/java/com/example/webviewdemo/MainActivity.java
+++ app/src/main/java/com/example/webviewdemo/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.webviewdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.webkit.WebViewCompat;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -26,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
         }
     }
+
+    private static final String TAG = "MainActivity";
     WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,16 +43,43 @@ public class MainActivity extends AppCompatActivity {
         webView   =findViewById(R.id.webview);
         String url = "https://blog.csdn.net/chentaishan?t=1";
         webView.loadUrl(url);
+
+        PackageInfo webViewPackageInfo = WebViewCompat.getCurrentWebViewPackage(this);
+        Log.d(TAG, "WebView version: " + webViewPackageInfo.versionName);
+
+
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
         webView.addJavascriptInterface(new WebAppInterface(this),"android");
 
-        webView.setWebViewClient(new WebViewClient(){});
+        webView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.equals(Uri.parse(url).getHost())) {
+                    // This is my website, so do not override; let my WebView load the page
+                    return false;
+                }
+
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+        });
         webView.setWebChromeClient(new WebChromeClient(){
+
 
         });
 
 //        myWebView.loadData(encodedHtml, "text/html", "base64");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
